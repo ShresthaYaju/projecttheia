81b477770f7685f26224199fe6dd9b3f67fa9b9a
Yaju Shrestha <shresthayazu@gmail.com>
2023-06-08 02:44:33 -0500

    add Modual Logic

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,29 +1,48 @@
 "use client";
 
+import { authModalState } from "@/atoms/authModalAtom";
 import React, { useRef } from "react";
 import { RxCross2 } from "react-icons/rx";
+import { useRecoilState } from "recoil";
 
 const AuthModal: React.FC = () => {
   const dialogRef = useRef<HTMLDialogElement>(null);
 
+  const [modalState, setModalState] =useRecoilState(authModalState);
+
+  // const handleOpen = () => {
+  //   dialogRef.current?.showModal()
+  //   
+  // }
+
+  const handleClose = () => {
+    setModalState((prev)=>({...prev, open: false}));
+    dialogRef.current?.close()
+  }
+
+  if (modalState.open===true  ) {
+    dialogRef.current?.close()
+    dialogRef.current?.showModal();
+  }
+ 
+
   return (
     <>
-      <button
-        className="btn-solid"
-        onClick={() => dialogRef.current?.showModal()}
-      >
-        Dialog
-      </button>
 
       <dialog
+  
         ref={dialogRef}
         className="rounded-lg p-3 bg-slate-100 text-black  w-4/5 sm:w-2/3 md:w-2/5 lg:w-2/6 xl:w-[28%]"
       >
         <div className="flex justify-between font-semibold text-lg">
-          <span>Modal Title</span>
+          <span>
+            {modalState.view==="login"&& "Log In"}
+            {modalState.view==="signup"&& "Sign Up"}
+            {modalState.view==="resetPassword"&& "Reset Password"}
+          </span>
           <button
             className="btn-outline"
-            onClick={() => dialogRef.current?.close()}
+            onClick={handleClose}
           >
             <RxCross2 />
           </button>
@@ -31,10 +50,6 @@ const AuthModal: React.FC = () => {
 
         <div className="my-3">Here is the modal body</div>
 
-        <div className="flex justify-end ">
-          <button className="btn-solid">Button</button>
-          <button className="btn-outline">Button</button>
-        </div>
       </dialog>
     </>
   );
--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+"use client";
+import { authModalState } from "@/atoms/authModalAtom";
+import React, { use } from "react";
+import { useSetRecoilState } from "recoil";
 
 const AuthButtons: React.FC = () => {
+  const setAuthModalState= useSetRecoilState(authModalState);
   return (
     <>
       <button
         className="btn-outline h-7 hidden sm:flex w-[70px] md:w-28 justify-center items-center"
-        //   onClick={}
+          onClick={
+            ()=>setAuthModalState({open: true, view: "login"})
+          }
       >
         Log In
       </button>
       <button
         className="btn-solid h-7 hidden sm:flex w-[70px] md:w-28 justify-center items-center"
-        //   onClick={}
+        onClick={
+          ()=>setAuthModalState({open: true, view: "signup"})
+        }
       >
         Sign Up
       </button>
